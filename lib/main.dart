a99e84f420890e59b54581eab49aec12c5acb077
Celeste <clstacosta@hotmail.com>
2022-04-25 19:48:03 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_bmi/pages/input_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: InputPage(),
+    );
+  }
+}
+
